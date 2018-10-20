b631d8a270937e19cdfb3ddffd2a7d9cc23c1c80
Turner Gregory <turnerbgregory@gmail.com>
2018-10-20 15:34:38 -0500

    more changes

---
+++ OneFamily/App/OneFamily/childLogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  childLogInViewController.swift
+//  OneFamily
+//
+//  Created by Turner Gregory on 10/20/18.
+//  Copyright © 2018 Turner. All rights reserved.
+//
+
+import UIKit
+
+class childLogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
