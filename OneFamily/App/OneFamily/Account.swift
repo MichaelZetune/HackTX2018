ecbf8d1d16208e6fefd74d18760000eed00a3715
Turner Gregory <turnerbgregory@gmail.com>
2018-10-20 14:13:11 -0500

    first ios commit

---
+++ OneFamily/App/OneFamily/Account.swift
@@ -0,0 +1,15 @@
+//
+//  Account.swift
+//  OneFamily
+//
+//  Created by Turner Gregory on 10/20/18.
+//  Copyright © 2018 Turner. All rights reserved.
+//
+
+import Foundation
+
+class Account {
+    
+    var name: String?
+    var balance: String?
+}
